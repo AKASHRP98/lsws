9f08164167b72362db8abd8fa7897da18283d2b1
Akash <kashthus@gmail.com>
2022-02-18 15:28:10 +0530

    geoip

--- 7G_log.php
+++ 7G_log.php
@@ -116,7 +116,19 @@ function perishablePress_7G_exit() {
 	
 	http_response_code(SEVENGSTATUS);
 	
-	exit(SEVENGEXIT);
+	exit("<!DOCTYPE html>
+<html style=\"height:100%\">
+<head>
+<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+<title> 403 Forbidden
+</title></head>
+<body style=\"color: #444; margin:0;font: normal 14px/20px Arial, Helvetica, sans-serif; height:100%; background-color: #fff;\">
+<div style=\"height:auto; min-height:100%; \">     <div style=\"text-align: center; width:800px; margin-left: -400px; position:absolute; top: 30%; left:50%;\">
+        <h1 style=\"margin:0; font-size:150px; line-height:150px; font-weight:bold;\">403</h1>
+<h2 style=\"margin-top:20px;font-size: 30px;\">Forbidden By 7G Firewall
+</h2>
+<p>Access to this resource on the server is denied!</p>
+</div></div></body></html>");
 	
 }
 
@@ -207,7 +219,6 @@ function perishablePress_7G_http_referrer() {
 }
 
 function perishablePress_7G_get_patterns($string, $match,$blockedBy) {
-	echo $blockedBy;
 	$patterns = explode('___', $match);
 	
 	foreach ($patterns as $pattern) {
